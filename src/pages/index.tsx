9e02ed23a7e206cda79a4f7b35294ddef333a06e
shochin666 <shoogawa111@gmail.com>
2022-09-12 01:03:58 +0900

    細かな実装を想定しCSSファイルを作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,10 @@ import { FiMail } from "react-icons/fi";
 
 import Icon from "../../public/Icon.jpeg";
 import { useEffect, useState } from "react";
+
+import BackGround from "../../public/Background.png";
+import BackGroundDecoration from "../../public/BackgroundDecoration.png";
+import styles from "../styles/index.module.css";
 // import Head from "next/head";
 // import Image from "next/image";
 // import styles from "../styles/Home.module.css";
@@ -27,11 +31,21 @@ const Home: NextPage = () => {
     { title: "kae8" },
     { title: "kae9" },
     { title: "kae10" },
+    { title: "kae11" },
+    { title: "kae12" },
+    { title: "kae13" },
+    { title: "kae14" },
+    { title: "kae15" },
+    { title: "kae16" },
+    { title: "kae17" },
+    { title: "kae18" },
+    { title: "kae19" },
+    { title: "kae20" },
   ];
 
   // 初期値は揃える必要がある
-  const [maxShowContents, setMaxShowContents] = useState(3);
-  const [addedlist, setAddedlist] = useState<hoge[]>(tmplist.slice(0, 3));
+  const [maxShowContents, setMaxShowContents] = useState(6);
+  const [addedlist, setAddedlist] = useState<hoge[]>(tmplist.slice(0, 6));
   const [newShowList, setNewShowList] = useState<hoge[]>([]);
 
   const hage = () => {
@@ -40,23 +54,36 @@ const Home: NextPage = () => {
     setMaxShowContents(maxShowContents + 3);
   };
   return (
-    <div>
-      <div className="bg-gradient-to-r from-pink-200 to-sky-300 w-full h-[1800px]">
+    <div className="font-ubuntu">
+      <div className="w-full h-[1800px]">
         <div className="w-full">
-          <div className="w-full h-[100px] flex justify-between">
-            <div>a</div>
+          <div className="w-full fixed top-0 h-[60px] flex justify-between mb-4 px-[5%] bg-white z-10">
+            <div>
+              <a
+                href="#"
+                className="absolute w-[320px] z-20 top-[20px] left-[32px]"
+              >
+                <img
+                  src={BackGroundDecoration.src}
+                  alt="ヘッダー"
+                  height={300}
+                  width={400}
+                  className="absolute z-20"
+                />
+                <p className="absolute z-30 top-[12px] left-[28px] text-white font-bold text-2xl">
+                  rev4 lab.
+                </p>
+              </a>
+            </div>
             <div>b</div>
           </div>
           <div className="flex mx-[20px]">
-            <div className="bg-white h-[1800px] w-[240px]"></div>
+            <div className="bg-gray-50 h-[1800px] w-[240px]"></div>
             <div>
-              <div
-                id="articles"
-                className="grid grid-cols-3 gap-[20px] h-min ml-[20px] mt-[200px] animate-pulse"
-              >
+              <div className="grid grid-cols-3 gap-[20px] h-min ml-[20px] mt-[200px] animate-pulse">
                 {addedlist.map((value) => {
                   return (
-                    <div className="bg-white bg-opacity-30 rounded-lg mx-auto h-[100px] w-[280px]">
+                    <div className="bg-white bg-opacity-30 rounded-lg mx-auto h-[100px] w-[280px] shadow-xl">
                       {value.title}
                     </div>
                   );
@@ -64,7 +91,7 @@ const Home: NextPage = () => {
 
                 {newShowList.map((value, i) => {
                   return (
-                    <div className="bg-white bg-opacity-30 rounded-lg mx-auto h-[100px] w-[280px]">
+                    <div className="bg-white bg-opacity-30 rounded-lg mx-auto h-[100px] w-[280px] shadow-lg">
                       {value.title}
                     </div>
                   );
@@ -80,7 +107,7 @@ const Home: NextPage = () => {
                 read more
               </button>
             </div>
-            <div className="fixed right-[20px] bg-white bg-opacity-30 rounded-lg w-[240px] flex flex-col py-[20px]">
+            <div className="fixed right-[20px] bg-gray-900 rounded-lg w-[240px] flex flex-col py-[20px] shadow-lg top-[80px]">
               <img
                 src={Icon.src}
                 alt="アイコン"
@@ -89,15 +116,15 @@ const Home: NextPage = () => {
                 className="rounded-full mx-auto"
               />
               <p className="text-white font-bold text-2xl text-center">rev4</p>
-              <p className="text-gray-600 text-sm mx-4 mt-6">
+              <p className="text-gray-400 text-sm mx-4 mt-6">
                 現在、物理学科に通う大学3年生。
                 <br />
                 PowerPlantを運営しており、フロントエンドをメインに個人開発しています。
                 <br />
               </p>
               <div className="flex justify-around text-white text-xl mt-4">
-                <a href="#" className="text-white hover:text-gray-100 text-2xl">
-                  <AiFillGithub />
+                <a href="#" className={styles.github__button}>
+                  <AiFillGithub className="text-2xl" />
                 </a>
                 <a href="#" className="text-blue-400 hover:text-blue-600">
                   <BsTwitter />
